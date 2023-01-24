964a85fbc61641e707fb27c3ecf8ad3acbe28a9d
Luke Hansen <80734674+lukeh1995@users.noreply.github.com>
2023-01-25 10:57:38 +1100

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title("My parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("Kale, Spinach and Rocket Smoothie")
-streamlit.text("Hard-Boilder Free-Range Egg") 
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
+streamlit.text("🐔 Hard-Boilded Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
