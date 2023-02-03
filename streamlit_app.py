3ed75d02c7d54bd4e2f2bf78dd59878d8da9748a
Luke Hansen <80734674+lukeh1995@users.noreply.github.com>
2023-02-03 11:36:36 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 streamlit.header("View our Fruit List - Add Your Favourites")
-  if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit Load List'):
                    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
                    my_data_rows = get_fruit_load_list()
                    my_cnx.close()
